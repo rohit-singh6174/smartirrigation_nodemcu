24be705c9ab9e1cebbe1a4dbefd6cc1ff40f46dc
rohit-singh6174 <66712860+rohit-singh6174@users.noreply.github.com>
2023-05-07 13:37:37 +0530

    Update smartirrigation.ino

--- smartirrigation.ino
+++ smartirrigation.ino
@@ -1,72 +1,76 @@
+#define BLYNK_PRINT Serial
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 #include <DHT.h>
-#define BLYNK_PRINT Serial
-char auth[] = "QOSXaduVtlUcTstpOqJYY7nQjB10oRm9";
-char ssid[] = "NPL";  // type your wifi name
-char pass[] = "CSRF3993";  // type your wifi password
+#include <Blynk.h>
 
-
-#define DHTPIN 2     // Digital pin connected to the DHT sensor
-
-#define DHTTYPE DHT11   // DHT 11
-//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+char auth[] = "<YourBlynk-API>";
+char ssid[] = "<YourWifi-SSID>";  // type your wifi name
+char pass[] = "<Wifi-Password>";  // type your wifi password of your Wi-Fi
+int water ;
+int prevState = 0;
+#define DHTPIN D1 
+#define DHTTYPE DHT11 
+BlynkTimer timer;
 DHT dht(DHTPIN, DHTTYPE);
 
 
-const int moisturePin = A0; // Analog pin for soil moisture sensor
-int moistureValue = 0; 
-BlynkTimer timer;
-float  wet=300;
-float  dry=1024;
-
-void sendSensor()
-{
-   float h = dht.readHumidity();
-  float t = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
- 
+void sendSensor(){
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
   if (isnan(h) || isnan(t)) {
     Serial.println("Failed to read from DHT sensor!");
     return;
   }
   // You can send any value at any time.
   // Please don't send more that 10 values per second.
- Blynk.virtualWrite(V5, t);
+  Blynk.virtualWrite(V5, t);
   Blynk.virtualWrite(V6, h);  
  
   Serial.println("Temperature: "+String(t)+"°C");
   Serial.println("Humidity: "+String(h)+"%");
+}
 
-   moistureValue = analogRead(moisturePin);
-   float percentage = (float)((moistureValue - wet)*100)/(dry - wet);
-
-   Blynk.virtualWrite(V0, percentage);
-   
-   if(percentage < 30.0){
-    Serial.println("Soil is Wet");
-   Serial.println(percentage);
-   }
-   else{
-     Serial.println("Soil is Dry  Water needed");
-     Serial.println(percentage);
-   }
+void setup()
+{  
+Serial.begin(9600);
+dht.begin();
+pinMode (D3,OUTPUT); //Waterpump
+pinMode (D5,INPUT); // Soil Mositure
+Blynk.begin(auth, ssid, pass);
+timer.setInterval(1000L, sendSensor);
 
 }
 
-void setup() {
-  Serial.begin(9600);
-  Serial.println(F("DHTxx test!"));
-  
-  Blynk.begin(auth, ssid, pass);
-  
-  dht.begin();
-  pinMode(moisturePin, INPUT);
-  timer.setInterval(1000L, sendSensor);
+void loop()
+{
+Blynk.run();
 
-}
 
-void loop() {
-  // put your main code here, to run repeatedly:
- Blynk.run();
-  timer.run();
+water=digitalRead(D5);
+Blynk.virtualWrite(V0,water);
+
+  if (water == HIGH && prevState == 0)
+  {
+  Serial.println("Pump Started, Water Flowing");
+  
+  digitalWrite(D3,LOW);
+
+  //Blynk.notify(“Pump Started, Water Flowing”);
+  prevState = 1;
+  delay(400);
+  }
+
+  else if (water == HIGH && prevState == 1)
+  {
+  Serial.println("Pump Stopped, Water Not Flowing");
+  delay(400);
+  }
+  else
+  {
+  digitalWrite(D3,HIGH);
+  prevState =0;
+  }
+delay(100);
+timer.run();
 }
